c14ac17338136275056eb335445a4247ed0d845f
Stefan Meichtry Blass <stefan.meichtry@fhnw.ch>
2025-03-17 22:37:27 +0100

    refactoring isActiveTetromino

--- src/modules/logic.mjs
+++ src/modules/logic.mjs
@@ -1,19 +1,14 @@
-function mark(model) {
-    const copy = structuredClone(model);
-    for (let row = 0; row < model.length; row++) {
-        for (let cell = 0; cell < model[row].length; cell++) {
-            if (0 < model[row][cell] && model[row][cell] < 10) {
-                copy[row][cell] = copy[row][cell] + 10;
-            }
-        }
-    }
-    return copy;
-}
+const mark = model =>
+    model.map(row => row.map(cell => (cell > 0 && cell < 10 ? cell + 10 : cell)));
+
+const isActiveTetromino = (model, row, cell) => (0 < model[row][cell] && model[row][cell] < 10)
+
+const hasCollision = model => !hasSpaceAround(model)
 
 function hasSpaceAround(model) {
     for (let row = model.length - 1; row >= 0; row--) {
         for (let cell = 0; cell < model[row].length; cell++) {
-            if (0 < model[row][cell] && model[row][cell] < 10) {
+            if (isActiveTetromino(model, row, cell)) {
                 if (row + 1 >= model.length || model[row + 1][cell] > 10) {
                     return false;
                 }
@@ -23,13 +18,11 @@ function hasSpaceAround(model) {
     return true;
 }
 
-const hasCollision = model => !hasSpaceAround(model)
-
 function move(model) {
     let copy = structuredClone(model);
     for (let row = model.length - 1; row >= 0; row--) {
         for (let cell = 0; cell < model[row].length; cell++) {
-            if (0 < model[row][cell] && model[row][cell] < 10) {
+            if (isActiveTetromino(model, row, cell)) {
                 if (hasSpaceAround(model)) {
                     copy[row + 1][cell] = model[row][cell]; // Move down
                     copy[row][cell] = 0; // Clear previous position
