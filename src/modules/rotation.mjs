d3cd65ef3530871241b08640d1af07bda3b4e254
Stefan Meichtry Blass <stefan.meichtry@fhnw.ch>
2025-05-11 15:35:50 +0200

    fs25: all tetrominos tested with pivots

--- src/modules/rotation.mjs
+++ src/modules/rotation.mjs
@@ -1,16 +1,6 @@
 import { x, y, activeTetromino } from '/src/modules/utils.mjs';
 import { applyTetromino } from './logic.mjs';
 
-const xs = tetromino => tetromino.map(x);
-const ys = tetromino => tetromino.map(y);
-
-const min = (tetromino, selector) => Math.min(...selector(tetromino));
-const max = (tetromino, selector) => Math.max(...selector(tetromino));
-
-const pivot = tetromino => 
-    [Math.round((min(tetromino, xs) + max(tetromino, xs)) / 2), 
-     Math.round((min(tetromino, ys) + max(tetromino, ys)) / 2)]
-
 const c90 = px => py => block =>
     [Math.round(-y(block) + py + px), Math.round(x(block) - px + py)]
 
@@ -23,4 +13,4 @@ const _rotate = (model, rotationFn) =>
 const rotateCW  = (px, py, model) => _rotate(model, c90(px)(py))
 const rotateCCW = (px, py, model) => _rotate(model, cc90(px)(py))
 
-export { rotateCW, rotateCCW, min, max, xs, ys, pivot };
+export { rotateCW, rotateCCW };
--- test/tests/rotation/rotation-tests.mjs
+++ test/tests/rotation/rotation-tests.mjs
@@ -1,59 +1,9 @@
 import { test, assert } from "/test/lib/unit-test.mjs"
-import { rotateCW, rotateCCW, xs, ys, min, max, pivot } from "/src/modules/rotation.mjs";
+import { rotateCW, rotateCCW } from "/src/modules/rotation.mjs";
 import { equals } from "/src/modules/logic.mjs";
-import { x, y } from "/src/modules/utils.mjs";
 
 function rotationTest() {
     const ROTATION = 'Rotation tests';
-
-    test('Min, max', () => {
-        // given
-        const tetromino = [
-            [10, 23],
-            [12, 26],
-            [14, 20],
-            [16, 29]
-        ];
-
-        // when
-        const minXResult = min(tetromino, xs);
-        const maxXResult = max(tetromino, xs);
-        const maxYResult = max(tetromino, ys);
-        const minYResult = min(tetromino, ys);
-
-
-        // then
-        assert(minXResult === 10, 'minXResult not correct');
-        assert(maxXResult === 16, 'maxXResult not correct');
-        assert(minYResult === 20, 'minYResult not correct');
-        assert(maxYResult === 29, 'maxYResult not correct');
-
-    }, ROTATION);
-
-    test('Pivot', () => {
-        // given
-        const tetromino = [
-            [4, 3], [4, 4], [4, 5], [5, 4]
-        ];
-        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 3, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 3, 3, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 3, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-        const piv = [4, 4];
-
-        // when
-        const result = pivot(tetromino);
-
-        // then
-        assert(x(result) === x(piv), "x not pivot");
-        assert(y(result) === y(piv), "y not pivot");
-
-    }, ROTATION);
 
     test('T, 90 CCW, CW, CCW', () => {
         // given
@@ -84,6 +34,249 @@ function rotationTest() {
         assert(equals(result, m2), "not correctly rotated");
 
     }, ROTATION);
+
+    test('J, CW, CW = 180', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 4, 0, 0, 0],
+            [0, 0, 4, 4, 4, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 4, 4, 4, 0],
+            [0, 0, 0, 0, 4, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCW(3, 2, m1);
+        result = rotateCW(3, 2, result);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('L, CW, CW, CW = 270', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 5, 0, 0, 0],
+            [0, 0, 5, 0, 0, 0],
+            [0, 0, 5, 5, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 5, 0, 0],
+            [0, 5, 5, 5, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCW(2, 2, m1);
+        result = rotateCW(2, 2, result);
+        result = rotateCW(2, 2, result);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('S, CCW, -90', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 6, 0, 0, 0],
+            [0, 0, 6, 6, 0, 0],
+            [0, 0, 0, 6, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 6, 6, 0, 0],
+            [0, 6, 6, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCCW(2, 2, m1);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('Z, CCW, CCW -180', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 7, 7, 0, 0, 0],
+            [0, 0, 7, 7, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 7, 7, 0, 0, 0],
+            [0, 0, 7, 7, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCCW(2, 3, m1);
+        result = rotateCCW(2, 3, result);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('I, CW = 90', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCW(1.5, 1.5, m1);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('I, CW, CW = 180', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        // when
+        let result = rotateCW(1.5, 1.5, m1);
+        result = rotateCW(1.5, 1.5, result);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('I, CW, CW, CW = 180', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+        const xOffset = 1;
+        const yOffset = 1;
+        const pivot = 1.5;
+        const px = xOffset + pivot;
+        const py = yOffset + pivot;
+
+        // when
+        let result = rotateCW(px, py, m1);
+        result = rotateCW(px, py, result);
+        result = rotateCW(px, py, result);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    test('O, CW = 90', () => {
+        // given
+        const m1 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 2, 2, 0, 0, 0],
+            [0, 2, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+
+        const m2 = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 2, 2, 0, 0, 0],
+            [0, 2, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+        ];
+        const xOffset = 1;
+        const yOffset = 2;
+        const pivot = 0.5;
+        const px = xOffset + pivot;
+        const py = yOffset + pivot;
+
+        // when
+        let result = rotateCW(px, py, m1);
+
+        // then
+        assert(equals(result, m2), "not correctly rotated");
+
+    }, ROTATION);
+
+    
 }
 
 export { rotationTest };
