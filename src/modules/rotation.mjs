7146327969d84d7787bbeddc90660319dfcd74f9
Stefan Meichtry Blass <stefan.meichtry@fhnw.ch>
2025-05-12 18:58:18 +0200

    fs25: first sketch of kicks

--- src/modules/rotation.mjs
+++ src/modules/rotation.mjs
@@ -4,10 +4,10 @@ import { cwStateChange, ccwStateChange } from '/src/modules/wall-kicks.mjs';
 const PIVOTS = { 1: 1.5, 2: 0.5  } // 1: I, 2: O
 const ALL_PIVOT_EXCEPT_I_O = 1;
 
-const c90 = px => py => block =>
+const _c90 = px => py => block =>
     [Math.round(-y(block) + py + px), Math.round(x(block) - px + py)]
 
-const cc90 = px => py => block =>
+const _cc90 = px => py => block =>
     [Math.round(y(block) - py + px), Math.round(-x(block) + px + py)]
 
 const _pivot = symbol => 
@@ -20,9 +20,9 @@ const _rotate = (model, rotationFn) =>
     applyTetromino(activeTetromino(model).map(rotationFn), model);
 
 const rotateCW  = (model) => 
-    _rotate(cwStateChange(model), _applyPivot(c90)(model.x)(model.y)(_pivot(type(model))))
+    _rotate(cwStateChange(model), _applyPivot(_c90)(model.x)(model.y)(_pivot(type(model))))
 
 const rotateCCW = (model) => 
-    _rotate(ccwStateChange(model), _applyPivot(cc90)(model.x)(model.y)(_pivot(type(model))))
+    _rotate(ccwStateChange(model), _applyPivot(_cc90)(model.x)(model.y)(_pivot(type(model))))
 
 export { rotateCW, rotateCCW };
