fa6dc50305ea54db76f3a533c9c9968b1a206da3
Stefan Meichtry Blass <stefan.meichtry@fhnw.ch>
2025-05-18 11:36:08 +0200

    fs25: single, double line clears tests defined

--- src/modules/line-clear.mjs
+++ src/modules/line-clear.mjs
@@ -9,3 +9,10 @@
 // 2. falls y_clear vollständig besetzt, Reihe löschen, Reihe y_clear wird frei
 // 3. alle Blöcke mit y < y_clear um y + 1 bewegen
 // 4. Wenn y_clear > 3, y_clear -= 1, weiter mit 2.
+
+const lineClear = model => {
+
+    return model;
+}
+
+export { lineClear };
--- test/tests/line-clear/line-clear-tests.mjs
+++ test/tests/line-clear/line-clear-tests.mjs
@@ -1,8 +1,10 @@
 import { test, assert } from "/test/lib/unit-test.mjs"
 import { spawn, TETROMINO_TYPE } from '/src/modules/spawn.mjs';
 import { equals, move, right } from '/src/modules/logic.mjs';
-import { rotateCCW } from '/src/modules/rotation.mjs';
+import { rotateCCW, rotateCW } from '/src/modules/rotation.mjs';
 import { applyMatrix, SPAWN_STATE, LEFT_STATE } from '/src/modules/utils.mjs';
+import { lineClear } from '/src/modules/line-clear.mjs';
+import { RIGHT_STATE } from "../../../src/modules/utils.mjs";
 
 function lineClearTest() {
     const LINE_CLEAR = 'Line clear tests';
@@ -48,6 +50,49 @@ function lineClearTest() {
         result = move(result);
         result = move(result);
         result = move(result);
+        result = lineClear(model);
+
+        // then
+        assert(equals(result, m2), 'Not a correct single line clear');
+
+    }, LINE_CLEAR);
+
+    test('Double', () => {
+        // given
+        const m1 = applyMatrix({ x: 0, y: 0, c: SPAWN_STATE, p: SPAWN_STATE, m: [
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0, 11],
+            [11, 11,  0, 11,  0,  0,  0, 11, 11, 11],
+            [11, 11, 11, 11,  0,  0, 11, 11, 11, 11],
+            [11, 11, 11, 11,  0, 11, 11, 11, 11, 11],
+        ]});
+
+        const m2 = applyMatrix({ x: 3, y: 5, c: RIGHT_STATE, p: SPAWN_STATE, m: [
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0,  0, 11],
+            [11, 11,  0, 11,  0, 17,  0, 11, 11, 11],
+        ]});
+        
+        // when
+        // spawn Z
+        // rotate cw 
+        // 5 x move
+        let result = spawn(m1)(TETROMINO_TYPE.Z);
+        result = rotateCW(result);
+        result = move(result);
+        result = move(result);
+        result = move(result);
+        result = move(result);
+        result = move(result);
+        result = lineClear(model);
 
         // then
         assert(equals(result, m2), 'Not a correct single line clear');
